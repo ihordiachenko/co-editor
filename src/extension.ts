cdd59a899e4a65e581eec64a065fe026654570c7
Ihor Diachenko <ih.diachenko@gmail.com>
2024-03-13 01:36:11 +0200

    Added the command

--- src/extension.ts
+++ src/extension.ts
@@ -13,9 +13,13 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('co-editor.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+	let disposable = vscode.commands.registerCommand('co-editor.proofread', () => {
+		// TODO:
+		// - get the current file
+		// - request the GPT API key
+		// - send the file to the GPT API
+		// - apply the changes to the file
+
 		vscode.window.showInformationMessage('Hello World from Co-editor!');
 	});
 
